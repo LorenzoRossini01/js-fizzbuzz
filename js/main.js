1be1aea54dd47f7828f11e90cddc7f03b8731cee
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-11 15:23:30 +0100

    fix: algorithm repetition

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+const rowContainer = document.getElementById("row-container");
+let boxText;
+
 for (let i = 1; i <= 30; i++) {
   if (i % 5 == 0 && i % 3 == 0) {
-    console.log("FizzBuzz");
+    boxText = "FizzBuzz";
   } else if (i % 5 == 0) {
-    console.log("Buzz");
+    boxText = "Buzz";
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    boxText = "Fizz";
   } else {
-    console.log(i);
+    boxText = i;
   }
+  console.log(boxText);
 }
